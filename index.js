2c27afb703bd6b350afcc6f208b9c5b4ea6ff91d
Chris-L985 <thelopezdispenser@gmail.com>
2021-06-27 20:52:24 -0600

    added license and badges to generateMarkdown

--- index.js
+++ index.js
@@ -5,98 +5,138 @@ const inquirer = require('inquirer');
 const generateMd = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    {
-      //Github Username
-      type: 'input',
-      name: 'username',
-      message: 'Enter your username',
-      validate: (usernameInput) => {
-        if (usernameInput) {
-          return true;
-        } else {
-          console.log("Enter your GitHub username.");
-          return false;
-        }
-      }
-    },
-    {
-      //Project title
-      type: 'input',
-      name: 'title',
-      message: 'What is the name of your project?',
-      validate: (titleInput) => {
-        if (titleInput) {
-          return true;
-        } else {
-          console.log ("Enter your project title.");
-          return false;
-        }
+const promptUser = () => {
+  return inquirer.prompt([
+      {
+        //Github Username
+        type: 'input',
+        name: 'username',
+        message: 'Enter your username',
+        validate: (usernameInput) => {
+          if (usernameInput) {
+            return true;
+          } else {
+            console.log("Enter your GitHub username.");
+            return false;
+          }
+        },
       },
-    },
-    {
-      //Project description
-      type: 'input',
-      name: 'description',
-      message: 'Describe your project.',
-      validate: (descriptionInput) => {
-        if (descriptionInput) {
-          return true;
-        } else {
-          console.log("Please provide a description of your project.");
-          return false;
-        }
-      }
-    },
-    {
-      // project usage
-      type: 'input',
-      name: 'usage',
-      message: 'Provide instructions and exammples for use.'
-    }, 
-    {
-      // built with section
-      type: 'checkbox',
-      name: 'stack',
-      message: 'What are the languages used on your application?',
-      choices: []
-    },
-    {
-      type: 'input',
-      name: 'credits',
-      message: 'List your collaborators.'
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Select the following licenses needed for your project',
-      choices: ["GNU GPLv3" , "ISC" , "Apache" , "No licence required"],
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the steps required to install your project?'
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Provide a listing of contributions as necessary.'
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'What are some steps to test your application?'
-    },
-]);
+      {
+        //Project title
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?',
+        validate: (titleInput) => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log ("Enter your project title.");
+            return false;
+          }
+        },
+      },
+      {
+        //Project description
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project.',
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log("Please provide a description of your project.");
+            return false;
+          }
+        },
+      },
+      {
+        // project usage
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and exammples for use.',
+        validate: (usageInput) => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log("Please provide your usage instructions.");
+            return false;
+          }
+        },
+      }, 
+      {
+        // built with section
+        type: 'checkbox',
+        name: 'stack',
+        message: 'What are the languages used on your application? (Check all that apply)',
+        choices: ['HTML', 'CSS', 'JavaScript', 'SQL' , 'ES6' , 'Bootstrap' , 'Node']
+      },
+      {
+        // Input Contributions
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators.'
+      },
+      {
+        // Input Licenses
+        type: 'list',
+        name: 'license',
+        message: 'Select the following licenses needed for your project',
+        choices: ["GNU GPLv3" , "ISC" , "Apache" , "No licence required"],
+      },
+      {
+        // Input Installation
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+        validate: (validateInput) => {
+          if (validateInput) {
+            return true
+          } else {
+            console.log("Please enter steps required to install your Application. (Enter N/A if no steps are required) ")
+            return false;
+          }
+        },
+      },
+      {
+        // Input for testing application
+        type: 'input',
+        name: 'tests',
+        message: 'What are some steps to test your application? (Type N/A if not applicable)',
+        validate: (testInput) => {
+          if (testInput) {
+            return true;
+          } else {
+            console.log("Please enter your steps for testing the application.");
+            return false;
+          }
+        },
+      },
+  ]);
+};
 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+  fs.writeFile("./README.md", data, (err) => {
+    if (err) {
+      console.log("There was an issue. Please try again");
+      return;
+    } else {
+      console.log("File created.");
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  promptUser().then((readmeData) => {
+    return generateMd(readmeData);
+  })
+  .then((createXML) => {
+    return writeToFile(createXML);
+  })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  var licenseBadge = "";
+  if (licenseBadge === "GNU GPLv3") {
+    licenseBadge = "https://img.shields.io/badge/License-GPLv3-blue.svg";
+
+  } else if (license === "ISC") {
+    licenseBadge = "https://img.shields.io/badge/License-ISC-blue.svg";
+
+  } else if (license === "Apache") {
+    licenseBadge = "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+  }
+
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  var licenseLink = "";
+  if (license === "GNU GPLv3") {
+    licenseLink = "https://www.gnu.org/licenses/gpl-3.0";
+
+  } else if (license === "ISC") {
+    licenseLink = "https://opensource.org/licenses/ISC";
+
+  } else if (license === "Apache") {
+    licenseLink = "https://www.apache.org/licenses/LICENSE-2.0";
+  }
+
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
